a6526ad0fe1f8f4626232af5b033cb82c956ffc5
snehasonkusare-tech <sonkusare.sneha15@gmail.com>
2026-02-19 19:51:21 -0500

    `Updated build output files with reduced build times`git push

---
+++ mini-grep/target/debug/deps/mini_grep-47e8922234216b28.d
@@ -0,0 +1,5 @@
+/Users/sneha/Desktop/Rust/mini-grep/target/debug/deps/mini_grep-47e8922234216b28.d: src/main.rs
+
+/Users/sneha/Desktop/Rust/mini-grep/target/debug/deps/libmini_grep-47e8922234216b28.rmeta: src/main.rs
+
+src/main.rs:
---
+++ mini-grep/target/debug/deps/mini_grep-8cdb6f1e10e99a7d.d
@@ -0,0 +1,5 @@
+/Users/sneha/Desktop/Rust/mini-grep/target/debug/deps/mini_grep-8cdb6f1e10e99a7d.d: src/main.rs
+
+/Users/sneha/Desktop/Rust/mini-grep/target/debug/deps/libmini_grep-8cdb6f1e10e99a7d.rmeta: src/main.rs
+
+src/main.rs:
